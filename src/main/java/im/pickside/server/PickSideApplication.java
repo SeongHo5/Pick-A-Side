1a1bdb21587b1f9f311bbd129ddd5af237de11bc
SeongHo5 <newfuring1123@gmail.com>
2024-03-28 16:31:43 +0900

    init: Init project

---
+++ src/main/java/im/pickside/server/PickSideApplication.java
@@ -0,0 +1,13 @@
+package im.pickside.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickSideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/im/pickside/server/PickSideApplicationTest.java
@@ -0,0 +1,12 @@
+package im.pickside.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickSideApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
